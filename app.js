acbe749b2e8761883a76a7e5d7402f726be0f05b
ray199229 <ray199229@gmail.com>
2023-02-23 23:00:48 +0800

    feat: add bcrypt & condition in register post

--- app.js
+++ app.js
@@ -20,9 +20,28 @@ app.get('/', (req, res) => {
    res.send('Hello')
 })
 
+// Create
+
+// Read
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// Update
+
+// Delete
+
 // 登入
 app.get('/users/login', (req, res) => {
-   res.render('login')
+   return Todo.findAll({
+      raw: true,
+      nest: true
+   })
+      .then((todos) => { return res.render('index', { todos:todos })})
+      .catch((error) => { return res.status(422).json(error)})
 })
 
 app.post('/users/login', (req, res) => {
@@ -36,8 +55,28 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const {name, email, password, confirmPassword} = req.body
-  User.create({ name, email, password })
-   .then(user => res.redirect('/'))
+  User.findOne({ where: {email} })
+   .then(user => {
+      if(user) {
+         console.log('User already exists')
+         return res.render('register', {
+            name,
+            email,
+            password,
+            confirmPassword
+         })
+      }
+      return bcrypt
+         .genSalt(10)
+         .then(salt => bcrypt.hash(password, salt))
+         .then(hash => User.create({
+            name,
+            email,
+            password
+         }))
+         .then(() => res.redirect('/'))
+         .catch(err => console.log(err))
+   })
 })
 
 // 登出
