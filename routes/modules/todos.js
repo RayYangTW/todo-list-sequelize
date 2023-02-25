be2216cab6edfffbb89f2c387dff7864c1e8a92c
ray199229 <ray199229@gmail.com>
2023-02-25 15:42:33 +0800

    feat: add update todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -27,6 +27,25 @@ router.get('/:id', (req, res) => {
 })
 
 // Update
+router.get('/:id/edit', (req, res) => {
+   const id = req.params.id
+   const userId = req.user.id
+   return Todo.findOne({ where : {id, userId}})
+      .then((todo) => { 
+         todo = todo.toJSON()
+         return res.render('edit', {todo})
+      })
+      .catch((err) => console.log(err))
+})
+
+router.put('/:id', (req, res) => {
+   const id = req.params.id
+   const userId = req.user.id
+   const { name, isDone } = req.body
+   return Todo.update( { name, isDone: (isDone === 'on') }, { where: {id, userId}})
+      .then(() => res.redirect('/'))
+      .catch((err) => console.log(err))
+})
 
 // Delete
 
