5128b28147d8dfeed9ffc23fea1fcf5d3d71cce2
ray199229 <ray199229@gmail.com>
2023-02-25 16:04:47 +0800

    refactor: modify home route to relate user and todos

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,11 +5,13 @@ const db = require('../../models')
 const Todo = db.Todo
 
 router.get('/', (req, res) => {
+   const userId = req.user.id
+   console.log(userId)
    return Todo.findAll({
       raw: true,
-      nest: true
-   })
-      .then((todos) => { return res.render('index', { todos:todos })})
+      nest: true,
+      where : { userId: userId }})
+      .then((todos) => { res.render('index', { todos })})
       .catch((error) => { return res.status(422).json(error)})
 })
 
